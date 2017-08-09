eead42d011e7d7fda887081018d8846e0d4e3a34
Andrew Phillips <skeledrew@gmail.com>
2017-08-09 13:32:25 -0500

    2017-08-09_13:32:25: auto-commit /home/aphillips5/code/custom_clfs.py with hash 2552249548

--- custom_clfs.py
+++ custom_clfs.py
@@ -3,11 +3,13 @@
 '''
 
 
+import itertools, random
+
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
-from sklearn.metrics import euclidean_distances
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 from common import *
 
@@ -24,7 +26,7 @@ class RulesBasedClassifier(BaseEstimator, ClassifierMixin):
         return 'RulesBasedClassifier(rules=%s, thresh_cnt=%d)' % (str(self._rules), self._thresh_cnt)
 
     def _default_rules(self):
-        rules = ['intoxicat\w+\b', 'banana bag', '(alcohol|etoh).+(dependence|withdrawal)', 'drink.{1,45}(every|per|each|/).*(day|night)', 'drunk', 'alcoholic', 'heavy (etoh|alcohol)']
+        rules = ['intoxicat\w+\b', 'banana bag', '(alcohol|etoh).{1,20}(dependence|withdrawal|abuse)', 'drink.{1,45}(every|per|each|/).*(day|night|daily)', 'drunk', 'alcoholic', 'heavy (etoh|alcohol)', 'CRITICAL_BAC']
         return rules
 
     def _rules_from_file(self, path):
@@ -42,11 +44,60 @@ class RulesBasedClassifier(BaseEstimator, ClassifierMixin):
         return rules
 
     def fit(self, X, y):
+        # generate rule and sample stats
         self._X = X
         self._y = y
+        re_stats = {}
+        self._samp_stats = []
+
+        for idx in range(len(X)):
+            samp = X[idx]
+            samp = '\n'.join(list(set(samp.split('\n'))))  # remove dups
+            samp = samp.replace('\n', ' ')
+            s_stat = [samp]
+
+            for pat_idx in range(len(self._rules)):
+                pat = self._rules[pat_idx]
+                if not pat in re_stats: re_stats[pat] = [1] * 4  # rfp, sfp, rfn, sfn
+                match_ctr = len(re.findall(pat, samp))
+                if match_ctr > 0:
+                    s_stat.append([pat, match_ctr])
+                    if y[idx] == 1: re_stats[pat][0] += match_ctr; re_stats[pat][1] += 1
+                    if y[idx] == 0: re_stats[pat][2] += match_ctr; re_stats[pat][3] += 1
+            self._samp_stats.append(s_stat)
+        self._re_stats = re_stats
         return self
 
     def predict(self, X):
+        preds = []
+        try:
+            if not isinstance(self._rules, list): return self._rules()
+
+        except Exception as e:
+            print(repr(e))
+            pdb.post_mortem()
+
+        for samp in X:
+            result = [1] * 4
+
+            for pat in self._re_stats:
+                match_ctr = len(re.findall(pat, samp))
+                if match_ctr > 0:
+                    stats = self._re_stats[pat]
+                    result[0] += stats[0] / stats[1]  # rf_pos/sf_pos
+                    result[1] += stats[2] / stats[3]  # rf_neg/sf_neg
+            result[2] = result[0] + result[1]
+            result[3] = result[0] - result[1]
+            #pdb.set_trace()
+
+            if result[0] / result[1] > 1:
+                preds.append(1)
+
+            else:
+                preds.append(0)
+        return preds
+
+    def predict_1(self, X):
         preds = []
         try:
             if not isinstance(self._rules, list): return self._rules()
@@ -67,6 +118,75 @@ class RulesBasedClassifier(BaseEstimator, ClassifierMixin):
                 preds.append(0)
         return preds
 
+class OptimizedRulesSeeker(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, rand_state=0, search=10, optimum='f1'):
+        self._rand_state = rand_state  # seed to randomize search
+        self._search = search  # %age of samples to get rules from
+        self._optimum = optimum  # score to optimize for
+
+    def fit(self, X, y):
+        try:
+            self._X = X
+            self._y = y
+            regexes = self._generate_regexes()
+            self._clf = RulesBasedClassifier(regexes).fit(X, y)
+            pdb.set_trace()
+
+        except Exception as e:
+            print(repr(e))
+            pdb.post_mortem()
+        return self
+
+    def _generate_regexes(self):
+        word_tok = r'((\b[a-zA-Z0-9_/\-&]+\b)|(\b\(d*,?d+)+(\.?\d+)?%?\w*\*?\b))'
+        word_sep = re.compile(r'[\s;.]')
+        sent_sep = re.compile(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s")
+        regexes = []
+        targ_samps = range(len(self._X)) ##self._set_targs(len(self._X), self._rand_state, self._search)
+
+        for idx in targ_samps:
+            samp = self._X[idx]
+            samp = samp.replace('\n', ' ')
+            sents = sent_sep.split(samp)
+
+            for sent in sents:
+                words = word_sep.split(sent)
+                regexes += self._res_from_words(words, word_tok)
+        return list(set(regexes))
+
+    def _res_from_words(self, words, tok):
+        targ_len = len(words) - 2
+        combs = []  # word combos
+        res = []  # regexes
+
+        for idx in range(targ_len+3):
+            # create combinations of all words in the sentence
+            elems = [list(x) for x in itertools.combinations(words, idx)]
+            combs.extend(elems)
+
+        for idx in range(len(combs)):
+            # make each combo into a regex
+
+            if len(combs[idx]) == 1:
+                res.append(combs[idx][0])
+                continue
+            max_words_btwn = targ_len - len(combs[idx]) + 3
+            btwn_re = r'%s{,%d}' % (tok, max_words_btwn)
+            res.append(btwn_re.join(combs[idx]))
+        return res
+
+    def _set_targs(self, targ_size, rand, search):
+        size = int(search / targ_size * 100)
+        rng = range(targ_size)
+        if rand == 0: return rng[:size]
+        random.seed(rand)
+        return random.sample(rng, size)
+
+    def predict(self, X):
+        return self._clf.predict(X)
+
 if __name__ == '__main__':
     print('This module contains importable classifiers')
+pdb.set_trace()
 commit_me(dataDir + 'tracking.json')
