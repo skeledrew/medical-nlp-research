4ef22eac070355ac7bf4583f39c7fa2e0ddaf7e6
Andrew Phillips <skeledrew@gmail.com>
2017-09-05 01:20:52 -0500

    2017-09-05_01:20:52: auto-commit /home/aphillips5/code/get_patients.py with hash 1256032438

--- get_patients.py
+++ get_patients.py
@@ -31,7 +31,7 @@ def loadNotes(noteFile):
     print('Loaded %d note records' % cnt)
     return notes
 
-def saveNotes(noteList, destDir):
+def saveNotes(noteList, destDir, id_col=0):
     # write out
     cnt = 0
     err = 0
@@ -44,7 +44,7 @@ def saveNotes(noteList, destDir):
         cnt += 1
 
         try:
-            name = destDir + note[0] + '.txt'
+            name = destDir + note[id_col] + '.txt'
 
             with open(name, 'a') as f:
                 f.write(''.join(ch for ch in '||'.join(note) if ch in string.printable) + '\n')
@@ -54,20 +54,22 @@ def saveNotes(noteList, destDir):
             writeLog('%s: Error in %s: %s' % (currentTime(), sys.argv[0], str(e)))
 
         try:
-            audNotes[note[0]].append(note)
+            audNotes[note[id_col]].append(note)
 
         except KeyError:
             files += 1  # better to inc here
-            audNotes[note[0]] = []
-            audNotes[note[0]].append(note)
+            audNotes[note[id_col]] = []
+            audNotes[note[id_col]].append(note)
 
         except Exception as e:
             err += 1
-            writeLog('Error at %s in %s for MRN %s: %s'(currentTime(), sys.argv[0], str(note[0]), str(e)))
-    saveJson(audNotes, allNotes)
+            writeLog('Error at %s in %s for MRN %s: %s'(currentTime(), sys.argv[0], str(note[id_col]), str(e)))
+    saveJson(audNotes, adeAllNotes)
     print('Total records: %d, total created files: %d, total errors: %d' % (cnt, files, err))
 
 if __name__ == '__main__':
     notesFile = sys.argv[1] if len(sys.argv) > 1 and os.path.exists(sys.argv[1]) else patNotes
-    destDir = dataDir + 'note_files/'
-    saveNotes(loadNotes(notesFile), destDir)
+    destDir = sys.argv[2] if len(sys.argv) > 2 else dataDir + 'note_files/'
+    id_col = int(sys.argv[3]) if len(sys.argv) > 3 else 0
+    saveNotes(loadNotes(notesFile), destDir, id_col)
+commit_me(dataDir + 'tracking.json', 'get_patients.py')
