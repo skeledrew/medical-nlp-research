13fe3d19a68ee4c84291073f85ec99f87bda14ea
Andrew Phillips <skeledrew@gmail.com>
2017-09-11 17:50:10 -0500

    2017-09-11_17:50:10: auto-commit /home/aphillips5/code/peel.py with hash 917568365

--- peel.py
+++ peel.py
@@ -8,15 +8,26 @@ oldDir = dataDir + 'anc_notes/'
 newDir = dataDir + 'anc_notes_trim/'
 substances = ['alcohol', 'beer', 'wine', 'liquor', 'scotch', 'bourbon', 'cognac', 'vodka', 'gin', 'jack daniels', 'brandy', 'hennessy', 'remy', 'alcoholic beverage', 'jagermeister']
 posTriggers = ['etoh', 'disorient', 'syncope', 'impair', 'decrease', 'drink', 'deficit', 'thc', 'intoxicat', 'banana bag', 'lorazepam', 'ativan', ' b1', ' b6', 'thiamine', 'pyridoxine', 'multivitamin', 'bac ', 'bal ', 'dependence', 'heavy', 'admits', 'pint', 'shots', 'fifth', 'pack', 'binge drink']
-genTriggers = ['fall', 'fell', 'dizz', 'nausea', 'vomit', 'lethargic', 'drowsy', 'shot', 'sluggish', 'male', 'groggy', 'cans?', '\b.{,3}\d.{,3}oz', 'day', 'week', 'daily', 'weekly', 'bottle', 'glass(es)?', 'gallon', ]
+neuTriggers = ['fall', 'fell', 'dizz', 'nausea', 'vomit', 'lethargic', 'drowsy', 'shot', 'sluggish', 'male', 'groggy', 'cans?', '\b.{,3}\d.{,3}oz', 'day', 'week', 'daily', 'weekly', 'bottle', 'glass(es)?', 'gallon', ]
 negTriggers = ['denies', 'none detected', 'sober']
-triggers = substances + posTriggers + genTriggers + negTriggers
+triggers = substances + posTriggers + neuTriggers + negTriggers
+wv_seed = ['etoh', 'beer', 'alcohol', 'fifths', 'intoxicated']
+wv_number = 6
+wv_level = 3
 
 
 def grabTriggerSections(args):
     subDirs = ['yes/', 'no/']
     if len(args) > 1: global oldDir, newDir; oldDir = args[1]; newDir = args[2]
     ensureDirs(newDir + subDirs[0], newDir + subDirs[1])
+    opt_args = {}
+    if len(args) > 3: opt_args = str_to_dict(args[3])
+    global wv_seed, wv_number, wv_level, triggers
+    if 'seed' in opt_args and 'seed_sep' in opt_args: wv_seed = opt_args['seed'].split(opt_args['seed_sep'])
+    if 'number' in opt_args: wv_number = opt_args['number']
+    if 'level' in opt_args: wv_level = opt_args['level']
+    triggers += gen_triggers(wv_seed, wv_number, wv_level)
+    triggers = list(set(triggers))
 
     for subDir in subDirs:
         noteFiles = fileList(oldDir + subDir)
@@ -29,7 +40,7 @@ def grabTriggerSections(args):
 
                 for note in nf:
                     content.append(note.strip())
-            content = makeStruct(content)
+            if 'make_struct' in opt_args: content = makeStruct(content)
 
             for line in content:
                 line = line.lower()  # lowercase every line
@@ -37,7 +48,6 @@ def grabTriggerSections(args):
                 try:
 
                     if line[-1] == ':' and len(line) > 2:
-                        #print(line)
                         meat.append(line)
                         continue
 
@@ -45,7 +55,7 @@ def grabTriggerSections(args):
                     # something's wrong with the line?
                     continue
 
-                for lNum in range(5):
+                '''for lNum in range(5):
                     lNum = ' ' + str(lNum) + ' '
                     subst = False
 
@@ -69,7 +79,7 @@ def grabTriggerSections(args):
                             break
 
                     if subst == True and hNum in line:
-                        line = line.replace(hNum, ' NUMBER_HIGH' + hNum)
+                        line = line.replace(hNum, ' NUMBER_HIGH' + hNum)'''
 
                 for trigger in triggers:
                     # search the words of interest
@@ -78,7 +88,6 @@ def grabTriggerSections(args):
                         # add lines with the target word
                         # -08-12 - change to regex search
                         meat.append(line)
-                        #print(line)
                         continue
 
             with open('%s%s%s' % (newDir, subDir, nfn), 'w') as nf:
@@ -108,13 +117,27 @@ def makeStruct(content):
             lines += label.strip() + ':'
     return lines
 
+def gen_triggers(seed, number, levels, seed_sep='|'):
+    words = seed if isinstance(seed, list) else seed.split(seed_sep)
+    curr_level = 0
+    number = int(number)
+    levels = int(levels)
+    ultra_triggers = []
+
+    while words:
+        ultra_triggers += Distance().find(words.pop(0), number, levels)
+    ultra_triggers = list(set(ultra_triggers))
+    return ultra_triggers
+
 if __name__ == '__main__':
     try:
-        print('%s: Working...' % currentTime())
+        writeLog('%s: Working...' % currentTime())
         grabTriggerSections(sys.argv)
-        print('%s: Trimming operation complete.' % currentTime())
+        msg = '%s: Trimming operation complete.' % currentTime()
+        slack_post(msg, '@aphillips')
+        writeLog(msg)
+        commit_me(dataDir + 'tracking.json', 'peel.py')
 
     except Exception as e:
         print('Exception: %s' % repr(e))
         pdb.post_mortem()
-commit_me(dataDir + 'tracking.json', 'peel.py')
