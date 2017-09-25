cf1f8cdb10e64f66d797b95d661fd7395099fded
Andrew Phillips <skeledrew@gmail.com>
2017-09-25 16:19:20 -0500

    Create new process for each crunch connection [UNTESTED]

--- crunch_server.py
+++ crunch_server.py
@@ -1,39 +1,42 @@
-#
+#! /home/aphillips5/envs/nlpenv/bin/python3
 
 
-import os, inspect
+import os, inspect, sys
 
 import rpyc
 
 
 class CrunchService(rpyc.Service):
 
-    def __init__(self):
-        super(CrunchService, self).__init__()
-        self.users_list = os.listdir('/home')
-
     def on_connect(self):
-        pass
+        self.users_list = os.listdir('/home')
 
     def on_disconnect(self):
         pass
 
     def invalid_user(self, user):
-        print('Invalid user {} tried to connect!'.format(user))
+        msg = 'Invalid user "%s" tried to connect!' % (user)
+        print(msg)
+
+        with open(os.environ['HOME'] + '/crunch_server.log', 'a') as fo:
+            fo.write(msg + '\n')
         return None
 
-    def exposed_run(self, obj, user):
-        if not user in self.users_list: return invalid_user(user)
-        if inspect.isfunction(obj): return obj()
+    def exposed_run(self, user, obj, args=[], kwargs={}):
+        if not user in self.users_list: return self.invalid_user(user)
+        if inspect.isfunction(obj): return obj(*args, **kwargs)
         if inspect.isclass(obj):
             obj.run()
             return obj
 
+    def exposed_cpu_count(self):
+        return os.cpu_count()
+
 if __name__ == '__main__':
     port = 9999 if len(sys.argv) < 2 else sys.argv[1]
-    from rpyc.utils.server import ThreadedServer
+    from rpyc.utils.server import ForkingServer
 
-    t = ThreadedServer(CrunchService, 'localhost', port=port)
+    t = ForkingServer(CrunchService, 'localhost', port=port)
     print('Running CrunchService. Use Ctrl+C to quit.')
     t.start()
     print('Killing CrunchService.')
