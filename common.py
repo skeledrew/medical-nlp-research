2f46fad5c3cbdaa6e86a0d008d5c9ff265bcd0bb
Andrew Phillips <skeledrew@gmail.com>
2017-09-25 23:18:47 -0500

    2017-09-25_23:18:46: auto-commit /home/aphillips5/code/common.py with hash 2513332951

--- common.py
+++ common.py
@@ -275,7 +275,7 @@ class CrunchClient():
 
         for conn in self.connections:
             # find unused connection and...
-            if self.connections[conn][1] == 'busy':
+            if self.connections[conn][1] in ['busy', 'ready']:
                 try:
                     # make sure the connection is still live
                     self.connections[conn][0].ping()
@@ -286,8 +286,8 @@ class CrunchClient():
                     if not res == 'ready': continue  # currently unusable
             user = conn.split(':')[2]
             if self._aborting: continue
-            if self.connections[conn][1] == 'dead': self.make_link(conn)
-            if not self.connections[conn][1] == 'ready': continue
+            if self.connections[conn][1] is 'dead': self.make_link(conn)
+            if not self.connections[conn][1] is 'ready': continue
 
             for idx, pending in enumerate(self.task_list):
                 # ... run the next pending task
