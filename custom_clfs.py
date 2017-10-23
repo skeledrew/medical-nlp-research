bc15d880a84541a7bebd5cb7e57e19b52b9703c1
Andrew Phillips <skeledrew@gmail.com>
2017-10-22 23:07:22 -0500

    2017-10-22_23:07:22: auto-commit /home/aphillips5/code/custom_clfs.py with hash 1698578616

--- custom_clfs.py
+++ custom_clfs.py
@@ -233,9 +233,7 @@ class BitVectorizor():
 
         for idx, bv in enumerate(self._bit_matrix):
             # make all bit vectors the same length
-            #diff = max_size - len(bv)
-            #pad = ''.join(['0'] * diff)
-            self._bit_matrix[idx] = bv.ljust(max_len, '0')#int(bv + pad, 2)
+            self._bit_matrix[idx] = int(bv.ljust(max_len, '0'), 2)
         return self._bit_matrix
 
     def _make_numbers(self):
