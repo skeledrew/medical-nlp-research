340eaa196388e660c9ad1f7d6ec25b8337eb2126
Andrew Phillips <skeledrew@gmail.com>
2017-10-26 21:54:43 -0500

    2017-10-26_21:54:43: auto-commit /home/aphillips5/code/f_bow.py with hash 3722367067

--- f_bow.py
+++ f_bow.py
@@ -17,7 +17,7 @@ import custom_clfs
 
 
 ERROR_IGNORE = 'ValueError..eta0||TypeError..sequence||Failed to create||ValueError..Unsupported set||TypeError..A sparse matrix'
-DEBUG = True
+DEBUG = False
 IGNORE = '~~IGNORE_THIS_PARAM~~'
 numCalls = 300  # number of calls; TODO: facilitate passing call num to called function
 memo = {}  # for memoization
