3ed51abc663b49e30bd597324a40f3e1b09243f9
Andrew Phillips <skeledrew@gmail.com>
2017-11-09 15:17:14 -0600

    2017-11-09_15:17:13: auto-commit /home/aphillips5/code/f_bow.py with hash 1655906867

--- f_bow.py
+++ f_bow.py
@@ -81,7 +81,7 @@ def gSGenericRunner(
     lc_params['train_set'] = notesDirName
     lc_params['mode'] = modSel
     lc_params['assoc_data'] = result
-    p, r, f1, std, mis, raw = CrossVal(numFolds, classifier, matrix, bunch, preproc_hash, clf_hash, result['features'], sk_feats) if modSel == 'kf' else learning_curve(numFolds, classifier, matrix, bunch, preproc_hash, clf_hash, result['features'], sk_feats, lc_params) #TTS(randState, classifier, matrix, bunch, preproc_hash, clf_hash)
+    p, r, f1, std, mis, raw = CrossVal(numFolds, classifier, matrix, bunch, preproc_hash, clf_hash, result['features'], sk_feats, **rest_kw) if modSel == 'kf' else learning_curve(numFolds, classifier, matrix, bunch, preproc_hash, clf_hash, result['features'], sk_feats, lc_params) #TTS(randState, classifier, matrix, bunch, preproc_hash, clf_hash)
     result['precision'] = p
     result['recall'] = r
     result['f1'] = f1
