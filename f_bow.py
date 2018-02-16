0c4631d4acd3bfc4575166e9c579f1bacecb7f6e
Andrew Phillips <skeledrew@gmail.com>
2018-02-16 14:26:17 -0600

    2018-02-16_14:26:17: auto-commit /home/aphillips5/code/f_bow.py with hash 914983280

--- f_bow.py
+++ f_bow.py
@@ -372,13 +372,12 @@ def GetMisses(y_test, pred, names):
 def main(args):
     state = Group()
     state('args', get_args())
-    if len(args) > 2 and args[1] == 'test': return test_eval(args[2:])
+    if state.args.test: return test_eval(state)
     s_time = currentTime()
-    pdb.set_trace()
     cfg_file = state.args.config_file or DEFAULT_CONFIG
     writeLog('Loading state from %s' % cfg_file)
     config = load_yaml(cfg_file)
-    gsParams = config.get('gsParams')
+    gSParams = config.get('gsParams')
     global memo#, gSParams
     g_size = 1
     sess_hash = hash_sum(str(gSParams))
@@ -777,7 +776,7 @@ def preproc_test(test_set, pipe):
 def get_args():
     from argparse import ArgumentParser as AP
     p = AP(description='Train and evaluate different model configurations')
-    p.add_argument('--config-file', help='Config file path', type=str, default=config)
+    p.add_argument('--config-file', help='Config file path', type=str, default=DEFAULT_CONFIG)
     p.add_argument('--learning-curve', help='Flag to generate a learning curve', action='store_true')
     p.add_argument('--test', help='Evaluate a model on a test set', action='store_true')
     p.add_argument('--test-dir', help='Test set path', type=str, default='')
