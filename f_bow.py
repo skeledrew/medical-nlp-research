360e348958985bbea95a449a208df9f0942238e4
Andrew Phillips <skeledrew@gmail.com>
2018-04-06 15:07:29 -0500

    2018-04-06_15:07:29: auto-commit /home/aphillips5/code/f_bow.py with hash 855915288

--- f_bow.py
+++ f_bow.py
@@ -514,7 +514,7 @@ def main(args):
     e_time = currentTime()
     save_err_msg = '' if not save_err else ' An error occurred during JSON save so YAML used instead.'
     grid_errs_msg = '' if not bad_grids else f' Skipped {len(bad_grids)} bad grids.'
-    fin_msg = 'Operation complete for experiment #%d. Started %s and ended %s. %s %s' % (
+    fin_msg = 'Operation complete for experiment #%d. Started %s and ended %s.%s%s' % (
         ex_num, s_time, e_time, save_err_msg, grid_errs_msg)
     writeLog(fin_msg)
     slack_post(fin_msg, '@aphillips')
