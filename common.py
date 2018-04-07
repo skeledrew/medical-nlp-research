7ba20d6ab636cec76a5952c6571e725d425f3750
Andrew Phillips <skeledrew@gmail.com>
2018-04-06 22:45:50 -0500

    Implement generic get_separated_values_file_iterator based on QuickUMLS's get_mrconso_iterator

--- common.py
+++ common.py
@@ -1160,7 +1160,6 @@ def get_separated_values_file_iterator(path, sep=',', headers=[], filter_='^[.*]
 
         for line in fo:
             if re.match(filter_, line): continue
-            if strip_last: line = line[:-1]
             line = line.strip()
             if line.endswith(sep): line = line[:-len(sep)]
             record = line.split(sep)
